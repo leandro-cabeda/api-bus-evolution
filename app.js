00725c4deac0bd6c9bdf4a5f9c3c45feb7d7b957
Leandro Cabeda Rigo <leandro.cabeda@hotmail.com>
2018-08-08 19:49:56 -0300

    atualizado uns dados dos gets e tokens

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const data = require('./teste_dados.json');
 const port = process.env.PORT || 6000;
 var request = require("request");
 const bodyParser = require("body-parser");
-const fs = require("file-system");
+const fs = require("fs");
 const httpclient = require("http-client");
 const path = require("path");
-const sqlite = require("sqlite");
+//const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET;
 
-//Importantes!!
+//Importantes inicio!!
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -26,14 +28,77 @@ app.get('/', function (req, res) {
 });
 
 
-//Importantes!
+//Importantes fim!
 
-// Exemplos abaixo!!
 
-app.get('/buscaTudo', function (req, res) {
-    res.send(data)
+// Exemplos inicio!!
+const redirectPath = "/api/auth";
+// Função de decodificação do login e senha
+function decodAuth(authorization) {
+    if (authorization === undefined) return [undefined, undefined]
+    return new Buffer(authorization.split(' ')[1], 'base64').toString().split(':');
+};
+
+app.get('/buscalinhas', function (req, res) {
+    res.send(data);
 });
-app.get('/stock/:itemid/:qtd', function (req, res) {
+
+pp.get('/api/auth', function (req, res) {
+    // Decodifica o valor de authorization, passado no header da requisição
+    let [username, password] = decodAuth(req.headers.authorization);
+
+        // Compara os valores de usuário e senha enviados com o objeto em memória
+        if (username === "leandro" && password === "963852741Lcr30") {
+            // Se o login e senha estiverem corretos gera o token com um tempo de vida de 1 hora
+            let token = jwt.sign({
+                login: "leandro"
+            }, secret,
+                { expiresIn: 60 * 60 });
+            // Responde com o status de logado como verdadeiro e o token gerado
+            res.status(200).json({
+                loged: true,
+                jwt: token
+            })
+        } else {
+            // Se não o login e senha não estiverem corretos seta o cabeçalho da resposta exigindo autenticação
+            res.set('WWW-Authenticate', 'Basic realm="401"');
+            // devolve o status de loged como falso
+            res.status(401).json({ loged: false, redirect: redirectPath });
+        }
+    
+});
+
+app.post('/api/token', function (req, res) {
+    // Recebe o token JWT pelo cabeçalho na chave autorization
+    let token = req.headers.authorization;
+    // Caso o token tenha valor
+    if (token) {
+        // remove a string "bearer"
+        token = req.headers.authorization.split(' ')[1];
+        // Usa a lib jwt para verificar a autenticidade do token
+        jwt.verify(token, secret, function (err, decoded) {
+            // Em caso de erro
+            if (err) {
+                // Responde que o usuário não está logado e informa o path para onde ele deve ir para se autenticar
+                res.status(401).json({ loged: false, redirect: redirectPath });
+            } else {
+                // Caso não haja problemas é porque o token é válido
+                // Adiciona o valor "loged" como verdadeiro
+                decoded.loged = true;
+                // devolve o payload do token para o requisitante
+                res.status(200).json(decoded);
+            }
+        });
+        // Caso não venha nada no header autorization
+    } else {
+        // Exige a autenticação do usuário
+        res.set('WWW-Authenticate', 'Bearer realm="401"');
+        // Envia o status de loged como falso e o endereço onde ele se autentica
+        res.status(401).json({ loged: false, redirect: redirectPath });
+    }
+});
+
+app.get('/stock/:itemid/:qtd', verifyToken, function (req, res) {
 
     let id = req.params.itemid;
     var qtdEstoque = req.params.qtd;
@@ -63,7 +128,7 @@ app.get('/item/:itemid', function (req, res) {
 });
 
 
-app.post("/stock/:itemid", function (req, res) {
+app.post("/stock/:itemid", verifyToken, function (req, res) {
     var id = req.params.itemid;
     var quantidade = parseInt(req.body.quantidade);
     var lista = data;
@@ -83,4 +148,27 @@ app.post("/stock/:itemid", function (req, res) {
 
     }
 });
-// Exemplos acima!!
+
+function verifyToken(req, res, next) {
+    let auth = req.headers.authorization;
+    if (auth) {
+        auth = auth.split(' ')[1];
+        let options = {
+            method: 'POST',
+            url: 'https://api-bus-evolution.herokuapp.com:80/api/token',
+            headers:
+                { Authorization: 'Bearer ' + auth }
+        }
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            body = JSON.parse(body);
+            if (body.loged) {
+                req.payload = body;
+            }
+            next();
+        })
+    } else {
+        next();
+    }
+};
+// Exemplos fim!!
